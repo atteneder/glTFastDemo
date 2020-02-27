c3431bc9100fde19ad3c9409a27b5612bbf56d59
Andreas Atteneder <andreas.atteneder@gmail.com>
2020-02-27 11:33:19 +0100

    Added scene menu and Sequential Mass Loader scene

---
+++ Assets/Scripts/SceneSelection.cs
@@ -0,0 +1,52 @@
+﻿// Copyright (c) 2019 Andreas Atteneder, All Rights Reserved.
+
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+
+//    http://www.apache.org/licenses/LICENSE-2.0
+
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSelection : MonoBehaviour
+{
+    [SerializeField] string[] scenes;
+    [SerializeField] float width = 300;
+    [SerializeField] float height = 100;
+    [SerializeField] float yGap = 10;
+    [SerializeField] bool alignRight;
+    [SerializeField] bool alignBottom;
+
+    bool hidden;
+
+    void Start() {
+        if(scenes!=null) {
+            var scene = SceneManager.GetActiveScene();
+            hidden = scene.name!=scenes[0];
+        }
+    }
+
+    void OnGUI() {
+        float x = alignRight ? Screen.width-width : 0;
+        float y = alignBottom ? Screen.height - (1+(hidden?0:scenes.Length))*(yGap+height) : yGap;
+
+        if(!hidden) {
+            foreach(var scene in scenes) {
+                if( GUI.Button( new Rect(x,y,width,height),scene)) {
+                    SceneManager.LoadScene(scene,LoadSceneMode.Single);
+                }
+                y += height + yGap;
+            }
+        }
+        if( GUI.Button( new Rect(x,y,width,height),hidden?"Menu":"Hide")) {
+            hidden = !hidden;
+        }
+    }
+}
---
+++ Assets/Scripts/SequentialMassLoader.cs
@@ -0,0 +1,105 @@
+﻿#if !(UNITY_ANDROID || UNITY_WEBGL) || UNITY_EDITOR
+#define LOCAL_LOADING
+#endif
+
+using System.Collections;
+using UnityEngine;
+
+public class SequentialMassLoader : MonoBehaviour
+{
+    [SerializeField]
+    GltfSampleSet[] sampleSets = null;
+
+    [SerializeField]
+    bool local = true;
+
+    bool waitForIt;
+    GLTFast.GltfAsset gltfAsset;
+
+    IEnumerator Start()
+    {
+        if(sampleSets!=null) {
+            foreach(var set in sampleSets) {
+                yield return set.Load();
+            }
+        }
+
+        // Wait a bit to make sure profiling works
+        yield return new WaitForSeconds(1);
+
+        float startTime = Time.realtimeSinceStartup;
+
+        // foreach (var n in GltfSampleModels.gltfFileUrls)
+        // {
+        //     yield return LoadIt<GLTFast.GltfAsset>(n,baseUrl);
+        // }
+
+        // foreach (var n in GltfSampleModels.glbFileUrls)
+        // {
+        //     yield return LoadIt<GLTFast.GltfAsset>(n,baseUrl);
+        // }
+
+        int count = 0;
+        if(sampleSets!=null) {
+            foreach(var set in sampleSets) {
+                if(set.items!=null) {
+                    if(local) {
+                        foreach(var item in set.itemsLocal) {
+                            yield return LoadIt<GLTFast.GltfAsset>(
+#if LOCAL_LOADING
+                                string.Format( "file://{0}", item.Item2)
+#else
+                                item.Item2
+#endif
+                            );
+                        }
+                        count++;
+                    } else {
+                        foreach(var item in set.items) {
+                            yield return LoadIt<GLTFast.GltfAsset>(item.Item2);
+                        }
+                        count++;
+                    }
+                }
+            }
+        }
+
+
+        Debug.LogFormat("Finished loading {1} glTFs in {0} seconds!",Time.realtimeSinceStartup-startTime,count);
+    }
+
+    IEnumerator LoadIt<T>(string n) where T:GLTFast.GltfAsset {
+        // var url = string.Format(
+        //     "{0}/{1}"
+        //     , baseUrl
+        //     , n
+        //     );
+        var url = n;
+        var go = new GameObject(System.IO.Path.GetFileNameWithoutExtension(url));
+        
+        Debug.Log(go.name);
+
+        waitForIt = true;
+
+        // GLTFast.GLTFast.LoadGlbFile( url, go.transform );
+        gltfAsset = go.AddComponent<T>();
+        gltfAsset.onLoadComplete += OnComplete;
+        gltfAsset.url = url;
+
+        while(waitForIt) {
+            yield return null;
+        }
+        Destroy(go);
+        yield return null;
+    }
+
+    void OnComplete(bool success) {
+        gltfAsset.onLoadComplete -= OnComplete;
+        if(!success) {
+            Debug.LogError("Ups");
+            Destroy(this);
+        } else {
+            waitForIt = false;
+        }
+    }
+}
