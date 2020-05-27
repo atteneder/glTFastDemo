4e3dd87fa6a07edf7d1cf70d9017bd600c4f199f
Andreas Atteneder <andreas.atteneder@gmail.com>
2020-05-28 01:03:46 +0200

    feat: Added custom header download tests

---
+++ Assets/Scripts/CustomHeaderGltfAsset.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using GLTFast;
+using GLTFast.Loading;
+
+public class CustomHeaderGltfAsset : MonoBehaviour
+{
+
+    public HttpHeader[] headers;
+
+    CustomHeaderDownloadProvider downloadProvider;
+
+    void Start() {
+        downloadProvider = new CustomHeaderDownloadProvider(headers);
+        var gltf = GetComponent<GltfAsset>();
+        gltf.Load(gltf.url,downloadProvider);
+    }
+}
