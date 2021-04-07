22f3be04f00bbc717762992c32b4ced5cccef07f
Andreas Atteneder <andreas.atteneder@gmail.com>
2021-04-07 15:07:08 +0200

    fix: Instantiation has to happen on main thread

--- Assets/Scripts/CustomLoadDemo.cs
+++ Assets/Scripts/CustomLoadDemo.cs
@@ -10,6 +10,11 @@ public class CustomLoadDemo : MonoBehaviour {
     
     // Start is called before the first frame update
     async void Start() {
+        await CustomInstantiation();
+        await CustomDeferAgent();
+    }
+
+    static async Task CustomInstantiation() {
         var gltf = new GLTFast.GLTFast();
         var success = await gltf.Load("file:///path/to/file.gltf");
 
@@ -19,15 +24,16 @@ public class CustomLoadDemo : MonoBehaviour {
             Debug.LogFormat("The first material is called {0}", material.name);
 
             // Instantiate the scene multiple times
-            gltf.InstantiateGltf( new GameObject("Instance 1").transform );
-            gltf.InstantiateGltf( new GameObject("Instance 2").transform );
-            gltf.InstantiateGltf( new GameObject("Instance 3").transform );
-        } else {
+            gltf.InstantiateGltf(new GameObject("Instance 1").transform);
+            gltf.InstantiateGltf(new GameObject("Instance 2").transform);
+            gltf.InstantiateGltf(new GameObject("Instance 3").transform);
+        }
+        else {
             Debug.LogError("Loading glTF failed!");
         }
     }
-    
-    async void CustomDeferAgent() {
+
+    async Task CustomDeferAgent() {
         // Recommended: Use a common defer agent across multiple GLTFast instances!
         
         // For a stable frame rate:
@@ -39,11 +45,14 @@ public class CustomLoadDemo : MonoBehaviour {
         
         foreach( var url in manyUrls) {
             var gltf = new GLTFast.GLTFast(null,deferAgent);
-            var task = gltf.Load(url).ContinueWith(t => {
-                if (t.Result) {
-                    gltf.InstantiateGltf(transform);
-                }
-            });
+            var task = gltf.Load(url).ContinueWith(
+                t => {
+                    if (t.Result) {
+                        gltf.InstantiateGltf(transform);
+                    }
+                },
+                TaskScheduler.FromCurrentSynchronizationContext()
+                );
             tasks.Add(task);
         }
 
