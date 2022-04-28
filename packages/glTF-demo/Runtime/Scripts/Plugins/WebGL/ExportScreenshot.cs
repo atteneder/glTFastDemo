9889aca8d0b83408bfedab0bbf6f7f1f75b45982
Felix Herbst <herbst@prefrontalcortex.de>
2022-04-28 19:12:15 +0200

    ifdef away Export functionality for now

--- packages/glTF-demo/Runtime/Scripts/Plugins/WebGL/ExportScreenshot.cs
+++ packages/glTF-demo/Runtime/Scripts/Plugins/WebGL/ExportScreenshot.cs
@@ -1,16 +1,24 @@
-﻿using System.Collections;
+﻿#if UNITY_GLTF
+#define HAVE_EXPORT_SUPPORT
+#endif
+
+#if HAVE_EXPORT_SUPPORT
+using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.InteropServices;
+#endif
 using UnityEngine;
 
 public class ExportScreenshot : MonoBehaviour {
 
     // from https://forum.unity.com/threads/user-image-download-from-in-webgl-app.474715/
-
+#if !HAVE_EXPORT_SUPPORT
+    public void DownloadScreenshot(string filename, Texture2D tex, bool noAutoClick) {}
+    public void DownloadJson(string filename, string json, bool noAutoClick) {}
+    public void DownloadGLB(string filename, byte[] glb, bool noAutoClick = false) {}
+#else
     [DllImport("__Internal")]
     private static extern void ImageDownloader(string str, string fn, string contentType, bool noAutoClick);
-
-    public static byte[] ssData = null;
 
     public Texture2D tex;
 
@@ -19,7 +27,7 @@ public class ExportScreenshot : MonoBehaviour {
         if (Application.isEditor)
             Debug.Log(o);
     }
-
+    
     public void DownloadScreenshot(string filename, Texture2D tex, bool noAutoClick)
     {
         var imageData = tex.EncodeToPNG();
@@ -52,4 +60,5 @@ public class ExportScreenshot : MonoBehaviour {
         ImageDownloader(System.Convert.ToBase64String(glb), filename, contentType, noAutoClick);
 #endif
     }
+#endif
 }
